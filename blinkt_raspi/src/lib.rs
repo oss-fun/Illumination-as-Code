70066d3f3e43f3ae2495c61ab1d8f3e3978da68d
sumeshi96 <yumapc96@gmail.com>
2024-06-13 17:51:38 +0900

    feat: httpクライアントの実装

--- blinkt_raspi/src/lib.rs
+++ blinkt_raspi/src/lib.rs
@@ -1,21 +1,50 @@
 use rand::Rng;
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
 
-pub fn set_rnd_color_chroma3(rng:&mut impl Rng) -> (u8, u8, u8) {
+#[derive(Serialize, Deserialize)]
+struct Chroma {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+pub fn set_rnd_color_chroma3(rng: &mut impl Rng) -> (u8, u8, u8) {
     let mut color: (u8, u8, u8) = (0, 0, 0);
-    let chromas = [0,127,255];
-    
+    let chromas = [0, 127, 255];
+
     color.0 = chromas[rng.gen_range(0..chromas.len())];
     color.1 = chromas[rng.gen_range(0..chromas.len())];
     color.2 = chromas[rng.gen_range(0..chromas.len())];
     color
 }
 
-pub fn set_rnd_color_chroma4(rng:&mut impl Rng) -> (u8, u8, u8) {
-    let mut color: (u8, u8, u8) = (0, 0, 0); 
-    let chromas = [0,85,170,255];
+pub fn set_rnd_color_chroma4(rng: &mut impl Rng) -> (u8, u8, u8) {
+    let mut color: (u8, u8, u8) = (0, 0, 0);
+    let chromas = [0, 85, 170, 255];
 
     color.0 = chromas[rng.gen_range(0..chromas.len())];
     color.1 = chromas[rng.gen_range(0..chromas.len())];
     color.2 = chromas[rng.gen_range(0..chromas.len())];
     color
+}
+
+pub async fn set_chroma_static(url: &str) -> Result<(u8, u8, u8), reqwest::Error> {
+    let client = Client::new();
+
+    match client
+        .post(url.to_string() + "/get-chroma-static")
+        .body("10")
+        .send()
+        .await
+    {
+        Ok(res) => {
+            let chroma: Chroma = res.json().await.unwrap();
+            Ok((chroma.red, chroma.green, chroma.blue))
+        }
+        Err(err) => {
+            println!("Request error: {}", err);
+            Err(err)
+        }
+    }
 }
--- blinkt_raspi/src/main.rs
+++ blinkt_raspi/src/main.rs
@@ -2,27 +2,29 @@ use std::error::Error;
 use std::thread;
 use std::time::Duration;
 
-use blinkt_raspi::{set_rnd_color_chroma4};
-
-use rand::{thread_rng};
-//use rand::seq::SliceRandom;
+use blinkt_raspi::set_chroma_static;
 
 use blinkt::Blinkt;
-
-fn main() -> Result<(), Box<dyn Error>> {
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let mut blinkt = Blinkt::new()?;
+    let url = "http://127.00.1:8080";
     loop {
-        let mut rng = thread_rng();
-        let (red, green, blue) = set_rnd_color_chroma4(&mut rng);
-        println!("R:{:03},G:{:03},B:{:03}",red,blue,green);
-        blinkt.set_pixel_rgbb(0, red, 0, 0, 0.1);
-        blinkt.set_pixel_rgbb(2, 0, green, 0, 0.1);
-        blinkt.set_pixel_rgbb(4, 0, 0, blue, 0.1);
-        blinkt.set_pixel_rgbb(7, red, green, blue, 0.1);
+        match set_chroma_static(url).await {
+            Ok((red, green, blue)) => {
+                println!("R:{:03},G:{:03},B:{:03}", red, green, blue);
+                blinkt.set_pixel_rgbb(0, red, 0, 0, 0.1);
+                blinkt.set_pixel_rgbb(2, 0, green, 0, 0.1);
+                blinkt.set_pixel_rgbb(4, 0, 0, blue, 0.1);
+                blinkt.set_pixel_rgbb(7, red, green, blue, 0.1);
 
-        blinkt.show()?;
+                blinkt.show()?;
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
 
         thread::sleep(Duration::from_millis(250));
     }
 }
-
