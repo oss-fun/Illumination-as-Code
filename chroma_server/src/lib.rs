d915045de6c718a7b0ca55b2ef089d6d7deaa5b4
sumeshi96 <yumapc96@gmail.com>
2024-06-23 13:25:51 +0900

    fix: 計算量に合わせてパーサを変更した

--- chroma_server/src/lib.rs
+++ chroma_server/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn parse_count_to_chroma(data: &str, color: &str, time: usize) -> u8 {
     let count: usize = data.match_indices(color).count();
 
-    let chroma: u8 = if (count / (time / 3) * 85) < 255 {
-        (count / (time / 3) * 85) as u8
+    let chroma: u8 = if (count / time * (time / 6) * 85) < 255 {
+        (count / time * (time / 6) * 85) as u8
     } else {
         255
     };
